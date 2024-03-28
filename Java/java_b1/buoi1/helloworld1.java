0c696daa6abfce315659260ba1d0460636224cd6
DgQuangDev <157321801+DgQuangDev@users.noreply.github.com>
2024-03-28 19:56:12 +0700

    Java_b1_btvn

--- Java/java_b1/buoi1/helloworld1.java
+++
@@ -1,9 +0,0 @@
-package buoi1;
-import java.util.Scanner;
-
-public class helloworld1{
-    public void xinchao(){
-        System.out.println("Xin chao");
-        System.out.println("--------------------------------");
-    }
-}
